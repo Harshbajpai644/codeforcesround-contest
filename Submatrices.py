9c990fcd6c1f59574bc062d5e47adfdf40e47280
Harsh Bajpai <harsh.bajpaics@gmail.com>
2026-03-18 20:38:30 +0530

    codeforces

---
+++ Submatrices.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for r in range(m):
+            for c in range(n):
+                if r > 0:
+                    grid[r][c] += grid[r-1][c]
+                if c > 0:
+                    grid[r][c] += grid[r][c-1]
+                if r > 0 and c > 0:
+                    grid[r][c] -= grid[r-1][c-1]
+                
+                if grid[r][c] <= k:
+                    count += 1
+                else:
+                    break
+        return count
